3c1750f31baaf18d27fd964e8c404314bd3d443e
vetsecglobal <veteransfuturefoundation@gmail.com>
2019-10-06 04:14:47 +0000

    first commit

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@ branch=$1
 
 if [[ ! -z ${KUBE_ENV} ]]
 then
-    git config remote.origin.url https://github.com/kevinstl/lnd-kube.git
+    git config remote.origin.url https://github.com/vetsecglobal/lnd-kube.git
     git config --global credential.helper store
     jx step git credentials
 
